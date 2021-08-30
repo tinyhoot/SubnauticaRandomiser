14bb7a834cce5ee7a5e19bc06f22875348354cbb
Raqzas <raqzas@gmail.com>
2021-08-30 14:09:42 +0200

    Dynamically add advanced settings to spoiler log.

--- SubnauticaRandomiser/RandomiserObjects/SpoilerLog.cs
+++ SubnauticaRandomiser/RandomiserObjects/SpoilerLog.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.IO;
+using System.Reflection;
 using System.Threading.Tasks;
 
 namespace SubnauticaRandomiser
@@ -10,9 +11,22 @@ namespace SubnauticaRandomiser
         internal static readonly string s_fileName = "spoilerlog.txt";
         private static readonly RandomiserConfig config = InitMod.s_config;
         internal static List<KeyValuePair<TechType, int>> s_progression = new List<KeyValuePair<TechType, int>>();
+        private static List<string> s_preparedAdvSettings;
         private static List<string> s_preparedDataboxes;
         private static List<string> s_preparedProgressionPath;
         private static string s_preparedMD5;
+
+        private static readonly List<string> s_basicOptions = new List<string>()
+        {
+            "iSeed",
+            "iRandomiserMode",
+            "bUseFish", "bUseEggs", "bUseSeeds",
+            "bRandomiseDataboxes",
+            "bVanillaUpgradeChains",
+            "bDoBaseTheming",
+            "iEquipmentAsIngredients", "iToolsAsIngredients", "iUpgradesAsIngredients",
+            "iMaxIngredientsPerRecipe", "iMaxAmountPerIngredient"
+        };
 
         private static string[] contentHeader =
         {
@@ -31,7 +45,14 @@ namespace SubnauticaRandomiser
             "Mode: " + config.iRandomiserMode,
             "Fish, Eggs, Seeds: " + config.bUseFish + ", " + config.bUseEggs + ", " + config.bUseSeeds,
             "Databoxes: " + config.bRandomiseDataboxes,
+            "Vanilla Upgrade Chains: " + config.bVanillaUpgradeChains,
+            "Base Theming: " + config.bDoBaseTheming,
             "Equipment, Tools, Upgrades: " + config.iEquipmentAsIngredients + ", " + config.iToolsAsIngredients + ", " + config.iUpgradesAsIngredients,
+            "Max Ingredients: " + config.iMaxIngredientsPerRecipe + " per recipe, " + config.iMaxAmountPerIngredient + " per ingredient",
+            ""
+        };
+        private static string[] contentAdvanced = 
+        {
             "",
             "",
             "///// Depth Progression Path /////"
@@ -42,6 +63,43 @@ namespace SubnauticaRandomiser
             "",
             "///// Databox Locations /////"
         };
+
+        // Add advanced settings to the spoiler log, but only if they have been
+        // modified.
+        private static void PrepareAdvancedSettings()
+        {
+            s_preparedAdvSettings = new List<string>();
+            FieldInfo[] defaultFieldInfoArray = typeof(ConfigDefaults).GetFields(BindingFlags.NonPublic | BindingFlags.Static);
+            FieldInfo[] fieldInfoArray = typeof(RandomiserConfig).GetFields(BindingFlags.Public | BindingFlags.Instance);
+
+            LogHandler.Debug("Number of fields in default, instance: " + defaultFieldInfoArray.Length + ", " + fieldInfoArray.Length);
+
+            foreach (FieldInfo defaultField in defaultFieldInfoArray)
+            {
+                // Check whether this field is an advanced config option or not.
+                if (s_basicOptions.Contains(defaultField.Name))
+                    continue;
+
+                foreach (FieldInfo field in fieldInfoArray)
+                {
+                    if (!field.Name.Equals(defaultField.Name))
+                        continue;
+
+                    var value = field.GetValue(config);
+
+                    // If the value of a config field does not correspond to its
+                    // default value, the user must have modified it. Add it to
+                    // the list in that case.
+                    if (!value.Equals(defaultField.GetValue(null)))
+                        s_preparedAdvSettings.Add(field.Name + ": " + value);
+
+                    break;
+                }
+            }
+
+            if (s_preparedAdvSettings.Count == 0)
+                s_preparedAdvSettings.Add("No advanced settings were modified.");
+        }
 
         // Grab the randomised boxes from masterDict, and sort them alphabetically.
         private static void PrepareDataboxes()
@@ -99,6 +157,7 @@ namespace SubnauticaRandomiser
 
         internal static async Task WriteLog()
         {
+            PrepareAdvancedSettings();
             PrepareDataboxes();
             PrepareMD5();
             PrepareProgressionPath();
@@ -108,6 +167,8 @@ namespace SubnauticaRandomiser
                 await WriteTextToLog(file, contentHeader);
                 await WriteTextToLog(file, s_preparedMD5);
                 await WriteTextToLog(file, contentBasics);
+                await WriteTextToLog(file, s_preparedAdvSettings.ToArray());
+                await WriteTextToLog(file, contentAdvanced);
                 await WriteTextToLog(file, s_preparedProgressionPath.ToArray());
                 await WriteTextToLog(file, contentDataboxes);
                 await WriteTextToLog(file, s_preparedDataboxes.ToArray());
