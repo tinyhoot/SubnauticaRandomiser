d9d0540c52c9fc5017067b2fe3d3d7c11a3b6f75
Raqzas <raqzas@gmail.com>
2021-12-16 21:18:22 +0100

    Introduce basic Entity skeleton.

---
+++ SubnauticaRandomiser/RandomiserObjects/LogicEntity.cs
@@ -0,0 +1,29 @@
+﻿using System;
+namespace SubnauticaRandomiser.RandomiserObjects
+{
+    public class LogicEntity
+    {
+        public readonly TechType TechType;
+        public readonly Blueprint Blueprint;    // For making it show up in the PDA
+        public RandomiserRecipe Recipe;         // For actually crafting it
+        public SpawnData SpawnData;             // For spawning it naturally in the world
+        public bool InLogic;                    // Is this available for randomising other entities?
+
+        /* 
+         * This class acts an abstract representation of anything that could or
+         * should be considered while randomising.       
+         * The Randomiser will pass over every one of these entities and only
+         * consider itself done once each of them has the InLogic flag - meaning
+         * that it is considered accessible within the game.
+         */        
+
+        public LogicEntity(TechType type, Blueprint blueprint = null, RandomiserRecipe recipe = null, SpawnData spawnData = null, bool inLogic = false)
+        {
+            TechType = type;
+            Blueprint = blueprint;
+            Recipe = recipe;
+            SpawnData = spawnData;
+            InLogic = inLogic;
+        }
+    }
+}
