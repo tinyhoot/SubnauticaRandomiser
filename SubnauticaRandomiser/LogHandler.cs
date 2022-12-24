d3f68a9e4ed9a62376389a3415916ada42c12cf5
tinyhoot <78366332+tinyhoot@users.noreply.github.com>
2022-12-24 12:45:07 +0100

    Delay in-game messages until the game has loaded.

--- SubnauticaRandomiser/LogHandler.cs
+++ SubnauticaRandomiser/LogHandler.cs
@@ -1,6 +1,12 @@
 ﻿using System;
+using System.Collections;
+using System.Collections.Generic;
 using BepInEx.Logging;
-using SubnauticaRandomiser.Interfaces;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using ILogHandler = SubnauticaRandomiser.Interfaces.ILogHandler;
+using Logger = BepInEx.Logging.Logger;
+
 namespace SubnauticaRandomiser
 {
     /// A class for handling all the logging that the main program might ever want to do.
@@ -9,6 +15,16 @@ namespace SubnauticaRandomiser
     internal class LogHandler : ILogHandler
     {
         private static readonly ManualLogSource _log = Logger.CreateLogSource("Randomiser");
+        private readonly List<string> _ingameMessages;
+        private bool _isReady;
+
+        public LogHandler()
+        {
+            _ingameMessages = new List<string>();
+            _isReady = false;
+            // Get notified as soon as the game has loaded and is ready to display in-game messages.
+            SceneManager.sceneLoaded += OnSceneLoaded;
+        }
 
         public void Debug(string message)
         {
@@ -39,11 +55,37 @@ namespace SubnauticaRandomiser
         public void InGameMessage(string message, bool error = false)
         {
             _log.LogMessage("Main Menu Message: " + message);
-            
-            if (error)
-                ErrorMessage.AddMessage("[ERROR] " + message);
+            if (!_isReady)
+                _ingameMessages.Add(message);
             else
                 ErrorMessage.AddMessage(message);
+        }
+
+        private IEnumerator DisplayDelayedMessages()
+        {
+            yield return new WaitForSeconds(3f);
+            _isReady = true;
+
+            foreach (string message in _ingameMessages)
+            {
+                ErrorMessage.AddMessage(message);
+            }
+            _ingameMessages.Clear();
+        }
+
+        /// <summary>
+        /// Get notified and proceed as soon as the main menu scene has loaded.
+        /// </summary>
+        private void OnSceneLoaded(Scene scene, LoadSceneMode mode)
+        {
+            if (scene.name != "XMenu")
+                return;
+
+            // Remove this event listener.
+            SceneManager.sceneLoaded -= OnSceneLoaded;
+            // Increase the vanilla fadeout time for messages.
+            ErrorMessage.main.timeDelay = 10f;
+            ErrorMessage.main.StartCoroutine(DisplayDelayedMessages());
         }
     }
 }
