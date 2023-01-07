e191a49eda61f74dd46f430ed6184b18252ff37f
tinyhoot <78366332+tinyhoot@users.noreply.github.com>
2023-01-07 21:00:55 +0100

    Emit warning if spawn is threatened by radiation.

--- SubnauticaRandomiser/Patches/AlternateStart.cs
+++ SubnauticaRandomiser/Patches/AlternateStart.cs
@@ -1,3 +1,4 @@
+using System;
 using HarmonyLib;
 using UnityEngine;
 
@@ -12,7 +13,7 @@ namespace SubnauticaRandomiser.Patches
         /// <param name="__result">The spawnpoint chosen by the game.</param>
         [HarmonyPostfix]
         [HarmonyPatch(typeof(RandomStart), nameof(RandomStart.GetRandomStartPoint))]
-        internal static void OverrideStart(ref Vector3 __result)
+        public static void OverrideStart(ref Vector3 __result)
         {
             if (__result.y > 50f)
                 // User is likely using Lifepod Unleashed, skip randomising in that case.
@@ -23,6 +24,27 @@ namespace SubnauticaRandomiser.Patches
 
             Initialiser._Log.Debug("[AS] Replacing lifepod spawnpoint with " + Initialiser._Serializer.StartPoint);
             __result = Initialiser._Serializer.StartPoint.ToUnityVector();
+        }
+
+        /// <summary>
+        /// Inform the player if their lifepod starts in a precarious, radiation-threatened position.
+        /// </summary>
+        [HarmonyPostfix]
+        [HarmonyPatch(typeof(EscapePod), nameof(EscapePod.StopIntroCinematic))]
+        public static void CheckRadiationRadius()
+        {
+            // Get the distance tracker from the Aurora's Radiation gameObject.
+            PlayerDistanceTracker tracker = LeakingRadiation.main.gameObject.GetComponent<PlayerDistanceTracker>();
+            float maxRadius = LeakingRadiation.main.kMaxRadius;
+
+            if (tracker.distanceToPlayer <= maxRadius)
+            {
+                float time = (maxRadius - LeakingRadiation.main.kStartRadius) / LeakingRadiation.main.kGrowRate;
+                int days = (int)Math.Floor(time / DayNightCycle.kDayLengthSeconds);
+                
+                Initialiser._Log.InGameMessage("CAUTION: You are inside the Aurora's radiation radius.");
+                Initialiser._Log.InGameMessage($"Radiation will reach the lifepod {days} days after explosion.");
+            }
         }
     }
 }
