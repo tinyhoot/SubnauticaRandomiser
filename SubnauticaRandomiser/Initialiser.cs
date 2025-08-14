4367787b6bf1c6f63e198d8bc398bc95a6a90e52
tinyhoot <78366332+tinyhoot@users.noreply.github.com>
2025-08-14 18:25:22 +0200

    Version bump to v0.13.0

--- SubnauticaRandomiser/Initialiser.cs
+++ SubnauticaRandomiser/Initialiser.cs
@@ -15,7 +15,7 @@ namespace SubnauticaRandomiser
     {
         public const string GUID = "com.github.tinyhoot.SubnauticaRandomiser";
         public const string NAME = "Subnautica Randomiser";
-        public const string VERSION = "0.12.1";
+        public const string VERSION = "0.13.0";
         
         // Files and structure.
         internal static Config _Config;
