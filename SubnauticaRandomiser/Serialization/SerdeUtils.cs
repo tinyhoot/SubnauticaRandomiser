6d49c4011f3871f6f090b50022fe3cb57d7f4dfb
tinyhoot <78366332+tinyhoot@users.noreply.github.com>
2026-02-27 18:27:29 +0100

    Add sync deserialiser.

--- SubnauticaRandomiser/Serialization/SerdeUtils.cs
+++ SubnauticaRandomiser/Serialization/SerdeUtils.cs
@@ -4,12 +4,52 @@ using System.IO;
 using System.Linq;
 using System.Threading.Tasks;
 using HootLib;
+using Nautilus.Json.Converters;
 using Newtonsoft.Json;
+using Newtonsoft.Json.Converters;
 
 namespace SubnauticaRandomiser.Serialization
 {
     internal static class SerdeUtils
     {
+        private static JsonSerializerSettings _defaultSettings = new JsonSerializerSettings
+        {
+            Converters =
+            {
+                new FloatConverter(),
+                new StringEnumConverter(),
+                new CustomEnumConverter(),
+                new Vector2Converter(),
+                new Vector2IntConverter(),
+                new Vector3Converter(),
+                new Vector3IntConverter()
+            }
+        };
+
+        /// <summary>
+        /// Creating new <see cref="JsonSerializerSettings"/> is expensive. This method returns cached general-purpose
+        /// settings with the most commonly needed converters.
+        /// </summary>
+        public static JsonSerializerSettings GetSettings(params JsonConverter[] converters)
+        {
+            if (converters.Length == 0)
+                return _defaultSettings;
+
+            return new JsonSerializerSettings
+            {
+                Converters = _defaultSettings.Converters.Concat(converters).ToList()
+            };
+        }
+
+        /// <summary>
+        /// Deserialise something all at once using the general-purpose <see cref="JsonSerializerSettings"/> settings.
+        /// </summary>
+        public static T DeserializeObject<T>(string json, params JsonConverter[] converters)
+        {
+            var settings = GetSettings(converters);
+            return JsonConvert.DeserializeObject<T>(json, settings);
+        }
+        
         /// <summary>
         /// Deserialise a collection of objects one at a time. Coroutine friendly.
         /// </summary>
@@ -21,7 +61,7 @@ namespace SubnauticaRandomiser.Serialization
             // Skip the initial StartArray token so we can deserialise object for object.
             reader.Read();
 
-            var serialiser = JsonSerializer.Create(new JsonSerializerSettings { Converters = converters });
+            var serialiser = JsonSerializer.Create(GetSettings(converters));
             while (reader.Read() && reader.TokenType != JsonToken.EndArray)
             {
                 // Doing this is only going to consume as much text as necessary to create another object.
