56c668286307998d1be0b9cbcd8486ba454acfc3
tinyhoot <78366332+tinyhoot@users.noreply.github.com>
2026-04-08 16:19:38 +0200

    Include classIds in spawnables.

--- SubnauticaRandomiser/Logic/LogicObjects/LogicSpawnable.cs
+++ SubnauticaRandomiser/Logic/LogicObjects/LogicSpawnable.cs
@@ -8,6 +8,15 @@ namespace SubnauticaRandomiser.Logic.LogicObjects
     internal class LogicSpawnable : LogicEntity
     {
         /// <summary>
+        /// The internal classIds used by the game to spawn any variations of this entity.
+        /// <br />
+        /// For fragments, this may be used for either the random spawner found in WorldEntities/Fragments or the
+        /// specific variants in WorldEntities/Environment/Wrecks. It is best to use whichever id is already present in
+        /// the vanilla game.
+        /// </summary>
+        public List<string> ClassIds = new List<string>();
+        
+        /// <summary>
         /// The biomes this entity should spawn in.
         /// </summary>
         public List<BiomeType> Spawns = new List<BiomeType>();
